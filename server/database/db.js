3255a7c993c0b6dccfde5e38ab7941075057d5fd
Suraj kumar <devilsuraj9641@gmail.com>
2023-09-08 14:47:43 +0530

    2nd

---
+++ server/database/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+
+
+
+const Connection = () => {
+
+    const MONGODB_URI = "mongodb://localhost:27017/suraj"
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
